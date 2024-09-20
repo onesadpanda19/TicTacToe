34ad4cf3a31ae597a601cbd53956f64565b7c43b
Andrew Nelson <nelsand19@gmail.com>
2024-09-19 18:45:32 -0600

    update playgame function with player prompts, start on updateGameBoard

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 /* first players choose a name, then player 1 (or x) goes first, then player 2 (o) goes next and game continues till one or the other wins or a tie. the result is then printed on screen*/
 /* set up game board in a three by three */
 
-let gameBoard = [];
+/*look at array manipulation/ user input temporary*/
+
+function createGame() {
+  const gameBoard = new Array(9).fill(null);
+  return gameBoard;
+}
+
+function playGame(choice) {
+  let playerName1 = prompt('choose a name');
+  let piece1 = prompt('choose x or o');
+
+  let playerName2 = prompt('choose a name');
+  let piece2 = prompt('choose x or o');
+
+  let playerOne = createPlayer(playerName1, piece1);
+  let playerTwo = createPlayer(playerName2, piece2);
+
+  console.log(playerOne);
+  console.log(playerTwo);
+
+  let userInput = prompt('Choose a number between 0 and 8');
+
+  console.log(userInput);
+}
+
+playGame();
 
 function createPlayer(playerName, piece) {
   if (piece == 'x') {
@@ -11,7 +36,8 @@ function createPlayer(playerName, piece) {
   }
 
   if (piece !== 'x' && piece !== 'o') {
-    return 'choose a valid choice!';
+    return 'make a valid choice!';
   }
+
   return { playerName, timesWon: 0, piece };
 }
